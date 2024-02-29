b825966c205285f20e69d2bb62eb15cab7123c9f
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-02-29 15:46:58 +0100

    add custom 404

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -19,7 +19,7 @@ export async function getStaticPaths() {
     const paths = getAllPostIds();
     return {
         paths,
-        fallback: false,
+        fallback: false,        // == any path not returned by static paths will send to 404 view
     };
 }
 
